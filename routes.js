35e747a7929852ebee66f44f8c7449206eae26ac
Jed Whitten <jed@whitten.org>
2014-10-05 04:18:10 +0000

    initial commit

---
+++ routes.js
@@ -0,0 +1,92 @@
+// server.js
+
+var router = require('express').Router();
+var nconf = require('nconf');
+var mysql = require('mysql');
+
+
+nconf.env();
+nconf.file({ file: 'config.json' });
+
+var connectionpool = mysql.createPool({
+    host     : nconf.get('mysql:host'),
+    user     : nconf.get('mysql:user'),
+    password : nconf.get('mysql:password'),
+    database : nconf.get('mysql:database')
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:23902/api)
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	console.log('Request received.');
+	next();
+});
+
+router.get('/', function(req, res) {
+	res.send('hello world!');	
+});
+
+
+router.get('/stars', function(req,res) {
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            connection.query('SELECT * FROM tblHYG ORDER BY StarId DESC LIMIT 20', function(err, rows, fields) {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                }
+                res.send({
+                    result: 'success',
+                    err:    '',
+                    data:   rows,
+                    length: rows.length
+                });
+                connection.release();
+            });
+        }
+    });
+});
+
+router.get('/stars/:id', function(req,res) {
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            connection.query('SELECT * FROM tblHYG WHERE StarId = ?', req.params.id, function(err, rows, fields) {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                }
+                res.send({
+                    result: 'success',
+                    err:    '',
+                    data:   rows,
+                    length: rows.length
+                });
+                connection.release();
+            });
+        }
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+
+var express    = require('express'); 	// call express
+var app        = express(); 		// define our app using express
+//var bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+
+var nconf = require('nconf');
+
+nconf.env();
+nconf.file({ file: 'config.json' });
+
+// all of our routes will be prefixed with /api
+app.use('/', require('./routes'));
+
+// START THE SERVER
+app.listen(nconf.get('http:port'));
+console.log('Server listening on port ' + nconf.get('http:port'));
