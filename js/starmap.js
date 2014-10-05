89c4c06d4efa4a8afb2f77c8f79d12c2b5bc4bff
Jed Whitten <jed@whitten.org>
2014-10-05 08:38:26 +0000

    star colors

--- js/starmap.js
+++ js/starmap.js
@@ -5,10 +5,43 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-function addStar(x, y, z, m) {
+function addStar(x, y, z, m, s) {
 	var size = 0.2 - m * 0.01;
 	var geometry = new THREE.SphereGeometry(size,32,32);
-	var material = new THREE.MeshBasicMaterial( { color: 0x00aaff } );
+	var star_color = 0xffffff, halo_color = 0xaaaaaa;
+	switch(s) {
+		case "O":
+			star_color = 0xaaccff;
+			halo_color = 0x0000ff;
+			break;
+		case "B":
+			star_color = 0xccccff;
+			halo_color = 0x0000ff;
+			break;
+		case "A":
+			star_color = 0xffffff;
+			halo_color = 0xffffff;
+			break;
+		case "F":
+			star_color = 0xffffff;
+			halo_color = 0xffffcc;
+			break;
+		case "G":
+			star_color = 0xffffcc;
+			halo_color = 0xe6e61a;
+			break;
+		case "K":
+			star_color = 0xffffcc;
+			halo_color = 0xe6941a;
+			break;
+		case "M":
+			star_color = 0xffddcc;
+			halo_color = 0xff0000;
+			break;
+	}
+
+
+	var material = new THREE.MeshBasicMaterial( { color: star_color } );
 	var star = new THREE.Mesh( geometry, material );
 	scene.add( star );
 	star.position.set(x, y, z)
@@ -17,22 +50,29 @@ function addStar(x, y, z, m) {
 	{ 
 		map: new THREE.ImageUtils.loadTexture( 'images/glow.png' ), 
 		useScreenCoordinates: false, 
-		color: 0x0000ff, transparent: false, blending: THREE.AdditiveBlending
+		color: halo_color, transparent: false, blending: THREE.AdditiveBlending
 	});
 	var sprite = new THREE.Sprite( spriteMaterial );
 	sprite.scale.set(size*15, size*15, size*15);
 	star.add(sprite); // this centers the glow at the mesh
 }
 
-$.getJSON('http://starmap.whitten.org/api/stars?xmin=-15&xmax=15&ymin=-15&ymax=15&zmin=-15&zmax=15', function (data) {
+$.getJSON('http://starmap.whitten.org/api/stars?xmin=-20&xmax=20&ymin=-20&ymax=20&zmin=-15&zmax=15', function (data) {
     var t = data.data;
     for(i = 0; i < t.length; i++) {
-        console.log(JSON.stringify(t[i]));
+        //console.log(JSON.stringify(t[i]));
 	x = -t[i].Y;
 	y = t[i].X;
 	z = 0.1 * t[i].Z;
         m = t[i].AbsMag;
-	addStar(x,y,z,m);
+        for (var s = ' ', j = 0; j < 3 && (s == ' ' || s == 'D'); j++) {
+            s = t[i].Spectrum.substring(0,1).toUpperCase();
+        }
+	if(s == ' ' && m > 8) {
+            s = 'M'; // probably a red dwarf
+        }
+        
+	addStar(x,y,z,m,s);
     }
 });
 
